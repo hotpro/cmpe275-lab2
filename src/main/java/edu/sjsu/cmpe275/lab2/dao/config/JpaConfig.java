79d6cfae68becb06c370702cd40e4cd4869e1031
Yutao Hou <chrishou118@gmail.com>
2016-03-29 19:14:38 -0700

    add mysql user

--- src/main/java/edu/sjsu/cmpe275/lab2/dao/config/JpaConfig.java
+++ src/main/java/edu/sjsu/cmpe275/lab2/dao/config/JpaConfig.java
@@ -50,9 +50,9 @@ public class JpaConfig {
     public DataSource dataSource() {
         final BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/spring_hibernate4_01?createDatabaseIfNotExist=true");
-        dataSource.setUsername("tutorialuser");
-        dataSource.setPassword("tutorialmy5ql");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/lab2?createDatabaseIfNotExist=true");
+        dataSource.setUsername("root");
+        dataSource.setPassword("cmpe275");
         dataSource.setInitialSize(2);
         dataSource.setMaxTotal(5);
 
